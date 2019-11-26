eabfc77f9a314453ffa95a69f9fd5a969a251ebc
Maoz M Shechtman <maozshechtman@gmail.com>
2019-11-26 21:28:08 +0200

    I added the buttom click listener.

--- app/src/main/java/com/ddb/uberdelivery/UI/MainActivity/MainActivity.java
+++ app/src/main/java/com/ddb/uberdelivery/UI/MainActivity/MainActivity.java
@@ -1,15 +1,76 @@
 package com.ddb.uberdelivery.UI.MainActivity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.ddb.uberdelivery.Entities.Parcel;
+import com.ddb.uberdelivery.Entities.Member;
 import com.ddb.uberdelivery.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+       private Button button;
+       private  TextView textView;
+    List<com.ddb.uberdelivery.Entities.Parcel> parcelList=new ArrayList<>();
+    List<Member> members= new ArrayList<>();
+    //region deleteAfter
+    String [] warehouseAddresses={"Htichon 11 Hifa","Moshe Solomon 3 Jerusalem","Hgamal 24 Beersheva"};
+    private void  fakeDetailes(){
+        members.add(new Member("051111113","Yair","git hub 3 Jerusalem"));
+        members.add(new Member("0525525223","Tal","kuzar 30 Jerusalem"));
+        members.add(new Member("0525525623","Tomer","None 13 Ariel"));
+
+    }
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.phonePlainText);
+        tv.setText("");
+        fakeDetailes();
+        button=(Button)(findViewById(R.id.addButton));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+
+                try
+                {
+                    ///todo:  ADD  THE Logical PART (PARCEL VALIDATION)
+                    Parcel parcel=new Parcel();
+                    parcelList.add(parcel);
+                  builder.setTitle("success");
+                  builder.setMessage("The parcel add to the FireBase");
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally {
+
+                    builder.create();
+                    builder.show();
+                }
+
+            }
+        });
+
+
+
     }
 }
